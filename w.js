67c38080ae78c1e4f14c9147c846a92aa7e25a80
zzemie <329rpodlas@frhsd.com>
2026-09-17 10:36:55 -0400

    Add initial JavaScript file with various scripts

---
+++ w.js
@@ -0,0 +1,49 @@
+ // ── Linked Scripts ──
+// /assets/in_layout_head2-7df9fd0fe690668e94972fa0d26f5cd2dcf253571e19f971e7aa0b0c89bb7165.js
+// https://cdn.weglot.com/weglot.min.js
+// /assets/application-01d0dc8f53eccfc356c07f9d109b81737ddb54e95a2e6a3ecdbf4260ab7fd2b4.js
+// /uploaded/themes/mod-east-hartford/main.js?1786355883
+
+dataLayer = [{ 'schoolDomain': 'howell.frhsd.com' }];
+
+// ── Next Inline Block ──
+
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+				new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+				j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+				'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+				})(window,document,'script','dataLayer','GTM-P3BGC7');
+
+// ── Next Inline Block ──
+
+document.getElementsByTagName('html')[0].className = document.getElementsByTagName('html')[0].className + ' wf-loading'
+
+// ── Next Inline Block ──
+
+Weglot.initialize({
+        api_key: 'wg_0741cda3e1bb71fc0ab892a82fbec5460'
+    });
+
+// ── Next Inline Block ──
+
+(function(window) {
+			window.FS.currentPage = {
+				dateFormat: 'md',
+				homepageVideoOptimization: true,
+				timeFormat: '12'
+			};
+			const settings = FS.getNS('settings');
+			settings.styleManagerEnabled = false;
+			settings.calendarsEnabled = false;
+			// Add A11ySettings to window for conditional loading
+			window.A11ySettings = {"search_element":false,"resource_element":false,"post_element":false,"post_tools":false,"location_element":false,"minor_enhancements":false,"calendar_element":false,"calendar_tools":false,"accordion_element":false,"feed_element":false,"issue_element":false,"cloudinary_video_player":false,"athletics_element":false,"constituent_element":false,"audioeye_admin":false};
+		})(window);
+
+			!function(){const b=function(){window.__AudioEyeSiteHash = '0eba4fcfed4fd671ec94088a24411728';
+				const a=document.createElement('script');
+				a.src='https://wsmcdn.audioeye.com/aem.js';
+				a.type='text/javascript';
+				a.async=!0;
+				a.onload = () => document.getElementById('fsPageContent').style.outline = 'none';
+				document.body.appendChild(a)};
+				window.addEventListener("load",b)}();
